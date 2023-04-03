2aaf82df3a88069e4fc72ace18a9b645c5fcd0fe
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2023-04-02 23:36:08 -0500

    props update

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -5,16 +5,18 @@ import HomeHero from '../components/HomeHero';
 import SignupForm from '../components/SignupForm';
 import Footer from '../components/Footer';
 
-export default function Signup()  {
+export default function Signup() {
     return (
         <>
-        <Navbar />
-        <HomeHero 
-            cName="hero-mid"
-            heroImg={"https://images.unsplash.com/photo-1529154036614-a60975f5c760?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"}
-        />
-        <SignupForm />
-        <Footer />
+            <Navbar />
+            <HomeHero
+                title="Signup"
+                btnClass="hide"
+                cName="hero-mid"
+                heroImg={"https://images.unsplash.com/photo-1529154036614-a60975f5c760?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"}
+            />
+            <SignupForm />
+            <Footer />
         </>
     )
 }
