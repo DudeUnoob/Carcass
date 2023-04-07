e1488047eaae490b15396de82107adcab2e42277
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2023-04-07 14:30:14 -0500

    server and geolocation api

--- src/functions/callGeoLocationAPI.js
+++ src/functions/callGeoLocationAPI.js
@@ -1,13 +1,18 @@
 import axios from "axios"
-const API_BASE_URL = `https://ipget.dudeunoob.repl.co`
+const API_BASE_URL = `https://carcassapi.dudeunoob.repl.co`
 
 export const callGeoLocationAPI = async() => {
-
+    const response = await axios.get("https://geolocation-db.com/json/");
     try {
-        const response = await axios.get("https://api.ipify.org/?format=json");
         
-        const secondResponse = await axios.post(`${API_BASE_URL}/api/v1/ip`, { ip: response.data.ip })
         
+        const secondResponse = await axios.post(`${API_BASE_URL}/api/v1/ip`, { ip: response.data }, {
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        
+        console.log(secondResponse.data)
         return secondResponse.data
     } 
     catch(error) {
