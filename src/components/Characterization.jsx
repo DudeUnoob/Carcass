a346aaef412e099a3c58f894c8faeb9bbba16e6f
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2023-04-08 18:11:01 -0500

    deleted commented lines

--- src/components/Characterization.jsx
+++ src/components/Characterization.jsx
@@ -57,35 +57,3 @@ const Characterization = () => {
 };
 
 export default Characterization;
-
-// function Destination() {
-//   return (
-//     <>
-//       <div className="destination">
-//         <h1>Popular Destinations</h1>
-//         <p>Tours give you the opportunity to see a lot, within a time frame.</p>
-//         <div className="first-des">
-//           <div className="des-text">
-//             <h2>Taal Volcano, Batangas</h2>
-//             <p>
-//               One of the most iconic views in Luzon, Mt. Taal boasts a volcano
-//               inside a lake inside an island. If you fancy a closer look, the
-//               hike up to the crater is a mere 45 minutes, and is easy enough for
-//               beginners. Guides will assist you most of the way, and you’ll see
-//               the peculiar environment found on an active volcano, including
-//               volcanic rocks and steam vents. The hike can be dusty and hot, so
-//               plan for an early morning trip, and then unwind with some bulalo
-//               before heading back home!
-//             </p>
-//           </div>
-//           <div className="image">
-//             <img src={Mountain1} alt="img" />
-//             <img src={Mountain2} alt="img" />
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Destination;
